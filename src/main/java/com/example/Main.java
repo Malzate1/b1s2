55c7d41135a9d1a6b0f05b84af3170db927c6713
Mayra Alzate <nadja.minni@gmail.com>
2025-07-21 18:36:28 -0500

    Instanciación de objetos estadio 1 y 2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,12 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
+        Estadio estadio1= new Estadio();
+        Estadio estadio2= new Estadio();
+
+
+        
+
        
     }
 }
