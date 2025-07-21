9b1f1cdbac7eabb1d348631fa6c363ba56939465
Mayra Alzate <nadja.minni@gmail.com>
2025-07-21 18:43:19 -0500

    Definición de métodos y adición de comentarios

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,11 +3,33 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
+        // Atributos
+
+
+
         Estadio estadio1= new Estadio();
         Estadio estadio2= new Estadio();
 
+        estadio1.nombre="Metropolitano";
+        estadio1.ciudad="Barranquilla";
+        estadio1.capacidad=46692;
 
-        
+        estadio2.nombre="El Campin";
+        estadio2.ciudad="Bogota";
+        estadio2.capacidad=39000;
+
+
+        // Métodos 
+
+        estadio1.mostrarInformacion();
+        estadio2.mostrarInformacion();
+
+
+
+
+
+
+
 
        
     }
